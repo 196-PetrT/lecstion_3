61b9dc13d7397177c2873bd7276e4fa8662c669e
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-08-26 21:09:32 +0300

    метод2 чтото принимает но не возвращает

---
+++ method2/Program.cs
@@ -0,0 +1,17 @@
+﻿// метод что-то принимает, но не возвращает
+
+/*void Method1(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method1(msg: "Автор Петр Т");
+*/
+void Method21(string msg, int count, int i = 0)
+{
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i ++; // инкремент (добавляет 1), если -- то декремент
+    }
+}
+Method21("Автор Петр Т", 4); // указываем что хотим увидеть " ", и сколько раз 4
