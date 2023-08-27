30a257cc89eea5128e5f0939293b3cb5c39b2b5d
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-08-27 15:11:06 +0300

    метод возвращает значение идентификатора но не принимает данные

---
+++ method3/Program.cs
@@ -0,0 +1,9 @@
+﻿// тип 3, задается идентификатор, который возвращает занчение но не принимает данные (аргументы)
+
+int Method3()
+{
+   
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
