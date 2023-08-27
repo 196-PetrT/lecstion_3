7aab634768299dbdbac7134433d8acf08ceb76e3
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-08-27 15:34:06 +0300

    метод возвращает значение идентификатора и принимает данные

---
+++ method4/Program.cs
@@ -0,0 +1,15 @@
+﻿// тип 4, задается идентификатор, который возвращает занчение и принимает данные (аргументы)
+
+string Method4(int count, string Symb)
+{
+    int i = 0;
+    string result = ""; // можно записать как string result = string.Empty , т.е. пустое значение
+    while (i < count)
+    {
+        result = result + Symb;
+        i++;
+    } 
+    return result;
+}
+ string abc = Method4(5, "abc"); // если навести на метод то он покажет что внутри и как заполнять
+Console.WriteLine(abc);
